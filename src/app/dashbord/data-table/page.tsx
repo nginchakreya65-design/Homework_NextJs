0be996d429d98b2903b0c85f917b3ca099d55d07
Ngin-Chakreya <nginchakreya65@gmail.com>
2026-09-14 15:49:50 +0700

    add new tomy  repo

--- src/app/dashbord/data-table/page.tsx
+++ src/app/dashbord/data-table/page.tsx
@@ -22,18 +22,14 @@ export type product = {
 }
 
 async function getData(): Promise<product[]> {
-  const res = await fetch("https://api.escuelajs.co/api/v1/products")
-
+  const res = await fetch("https://api.escuelajs.co/api/v1/products");
+  
   if (!res.ok) {
-    throw new Error("Failed to fetch products")
+    throw new Error("Failed to fetch products");
   }
 
-  const data = (await res.json()) as product[]
-
-  return data.map((item) => ({
-    ...item,
-    image: item.image ?? item.images?.[0] ?? "",
-  }))
+  const data = await res.json();
+  return data;
 }
 
 
